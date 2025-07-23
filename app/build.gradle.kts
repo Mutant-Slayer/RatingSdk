3d357cd8d1fa55bc6c99a24c5f5720a7905e35d8
anas shikoh <anas.shikoh@kukufm.com>
2025-07-23 18:05:40 +0530

    fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,6 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.material3)
-    implementation(project(":ratingSdk"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
