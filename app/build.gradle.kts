45a33af4dd7ed8b771136152cec917c82b45e49f
anas shikoh <anas.shikoh@kukufm.com>
2025-08-03 22:23:44 +0530

    migrate to toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation("com.github.Mutant-Slayer:RatingSdk:7f2315ac71")
+    implementation(libs.ratingSdk)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
